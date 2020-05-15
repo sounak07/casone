537fa7d1cdc77293a16f10c945c29bbae81bf4f8
sounak07 <sounakume@gmail.com>
2020-05-15 17:44:55 +0530

    Address part complete

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 /** @format */
 
-function formCreate(movie, id) {
+function formCreate(incomingData, id) {
   let output = `      
-        <input type="text" class="inputStyle" value=${movie[id].firstName} placeholder="First Name" >
-        <input type="text" class="inputStyle" value=${movie[id].lastName} placeholder="Last Name" />
-        <input type="text" class="inputStyle" value=${movie[id].Address1} placeholder="Address Line 1" />
-        <input type="text" class="inputStyle" value=${movie[id].Address2} placeholder="Address Line 2" />
-        <input type="text" class="inputStyle" value=${movie[id].City} placeholder="City" />
-        <input type="text" class="inputStyle" value=${movie[id].State} placeholder="State" />
-        <input type="number" class="inputStyle" value=${movie[id].Zipcode} placeholder="Zipcode" />
-        <input type="text" class="inputStyle" value=${movie[id].Country} placeholder="Country" />      
+        <input type="text" class="inputStyle" value=${incomingData[id].firstName} placeholder="First Name" >
+        <input type="text" class="inputStyle" value=${incomingData[id].lastName} placeholder="Last Name" />
+        <input type="text" class="inputStyle" value=${incomingData[id].Address1} placeholder="Address Line 1" />
+        <input type="text" class="inputStyle" value=${incomingData[id].Address2} placeholder="Address Line 2" />
+        <input type="text" class="inputStyle" value=${incomingData[id].City} placeholder="City" />
+        <input type="text" class="inputStyle" value=${incomingData[id].State} placeholder="State" />
+        <input type="number" class="inputStyle" value=${incomingData[id].Zipcode} placeholder="Zipcode" />
+        <input type="text" class="inputStyle" value=${incomingData[id].Country} placeholder="Country" />      
     `;
 
   return output;
@@ -19,15 +19,30 @@ function getAddress() {
   fetch(`https://raw.githubusercontent.com/sounak07/jsonData/master/data.json`)
     .then((response) => {
       response.json().then(function (data) {
-        let movie = data;
+        let shopping = data;
 
-        let bill = formCreate(movie, 0);
-        let ship = formCreate(movie, 1);
+        let bill = formCreate(shopping, 0);
+        let ship = formCreate(shopping, 1);
 
         var d1 = document.querySelector('#billDetails');
         var d2 = document.querySelector('#shipDetails');
         d1.insertAdjacentHTML('afterend', bill);
         d2.insertAdjacentHTML('afterend', ship);
+
+        let orderDate = shopping[2].OrderData;
+        let deliveryDate = shopping[3].ExpectedDelivery;
+
+        let dates = `
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+        <input type="text" class="dateField" value=${orderDate} placeholder="Order Date" />
+
+        <i class="fa fa-calendar" aria-hidden="true"></i>
+        <input type="text" class="dateField" value="${deliveryDate}" placeholder="Delivery Date" />
+
+        `;
+
+        var d3 = document.querySelector('.datedetails');
+        d3.insertAdjacentHTML('afterend', dates);
       });
     })
     .catch((e) => {
